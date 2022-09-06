46617867d1d8e2003238d568e52754b8079defcf
Ethan Straub <evstraub@gmail.com>
2022-09-06 13:23:57 -0400

    add game js and some html and css

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 //rock paper scissors
-const myArray = ["Rock", "Paper", "Scissors"];
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
-function computerChoice() {
-  return myArray[Math.floor(Math.random() * myArray.length)];
+//const myArray = ["ROCK", "PAPER", "SCISSORS"];
+
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerPlay();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+  })
+);
+
+function computerPlay() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randNum) {
+    case 1:
+      computer = "ROCK";
+      break;
+    case 2:
+      computer = "PAPER";
+      break;
+    case 3:
+      computer = "SCISSORS";
+      break;
+  }
 }
-console.log(computerChoice());
+function checkWinner() {
+  if (player == computer) {
+    return "Draw!";
+  } else if (computer == "ROCK") {
+    return player == "PAPER" ? "You Win!" : "You Lose!";
+  } else if (computer == "PAPER") {
+    return player == "SCISSORS" ? "You Win!" : "You Lose!";
+  } else if (computer == "SCISSORS") {
+    return player == "ROCK" ? "You Win!" : "You Lose!";
+  }
+}
